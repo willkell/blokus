de9ed0fffef0f4f704a2255166b85c8521cad375
= <=>
2022-07-14 16:18:03 -0400

    fixed concurrency bug

--- gameRunner.py
+++ gameRunner.py
@@ -16,34 +16,38 @@ def runGame():
     ).stdout.split("\n")
 
 
-numGames = 0
-scores = {1: [], 2: [], 3: [], 4: []}
-wins = {1: 0, 2: 0, 3: 0, 4: 0}
-times = []
-res = []
-with concurrent.futures.ProcessPoolExecutor() as executor:
-    results = [executor.submit(runGame) for _ in range(2000)]
+def main():
+    numGames = 0
+    scores = {1: [], 2: [], 3: [], 4: []}
+    wins = {1: 0, 2: 0, 3: 0, 4: 0}
+    times = []
+    res = []
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        results = [executor.submit(runGame) for _ in range(2000)]
 
-    for f in concurrent.futures.as_completed(results):
-        numGames += 1
-        print(f"Game {numGames} done")
-        res.append(f.result())
+        for f in concurrent.futures.as_completed(results):
+            numGames += 1
+            print(f"Game {numGames} done")
+            res.append(f.result())
+
+    for game in res:
+
+        for row in game:
+            if re.search("^Player [0-9]: [0-9]+$", row):
+                scores[int(row[7])].append(int(row[10:]))
+            elif re.search("^---", row):
+                gameTime = float(row[4:-12])
+                times.append(gameTime)
+            elif re.search("^Player [0-9] wins", row):
+                wins[int(row[7])] += 1
+    print("Number of games:", numGames)
+    print("Average Scores:")
+    for i in range(1, 5):
+        print("Player {} Average Score: {}".format(i, average(scores[i])))
+        print("Player {} Wins: {}".format(i, wins[i]))
+    print("Average Time: {}".format(average(times)))
+    print("Total Time: {}".format(time.time() - startTime))
 
 
-for game in res:
-
-    for row in game:
-        if re.search("^Player [0-9]: [0-9]+$", row):
-            scores[int(row[7])].append(int(row[10:]))
-        elif re.search("^---", row):
-            gameTime = float(row[4:-12])
-            times.append(gameTime)
-        elif re.search("^Player [0-9] wins", row):
-            wins[int(row[7])] += 1
-print("Number of games:", numGames)
-print("Average Scores:")
-for i in range(1, 5):
-    print("Player {} Average Score: {}".format(i, average(scores[i])))
-    print("Player {} Wins: {}".format(i, wins[i]))
-print("Average Time: {}".format(average(times)))
-print("Total Time: {}".format(time.time() - startTime))
+if __name__ == "__main__":
+    main()
